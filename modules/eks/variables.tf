b8f9aac95a97eef9bf07064b2ff1315193aca9fe
Thang Phan <thangjenny2002@gmail.com>
2026-01-06 21:18:13 +0700

    enhance naming convention of EKS module variables

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,33 +1,38 @@
+variable "aws_region" {
+  description = "AWS region where the EKS cluster will be deployed"
+  type        = string
+  default     = "ap-southeast-1"
+  nullable    = false
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "eks-learning-terraform"
-}
-
-variable "kubernetes_version" {
-  description = "Version of the EKS cluster"
-  type        = string
-  default     = "1.33"
+  nullable    = false
 }
 
 variable "environment" {
-  description = "Environment name"
+  description = "Environment name for resource tagging and naming"
   type        = string
+  nullable    = false
 }
 
-variable "aws_region" {
-  description = "AWS region to deploy resources"
+variable "kubernetes_version" {
+  description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "sa-east-1"
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
+  default     = "1.31"
+  nullable    = false
 }
 
 variable "private_subnet_ids" {
-  description = "Private subnet IDs"
+  description = "List of private subnet IDs where EKS worker nodes will be deployed"
   type        = list(string)
+  nullable    = false
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the EKS cluster will be created"
+  type        = string
+  nullable    = false
 }
 
